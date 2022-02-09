2100d8d5ec8f1c81af807bff1815e9a7be6f582f
WaffiqA <59906896+waffiqaziz@users.noreply.github.com>
2022-02-09 08:51:52 +0700

    merubah  format komentar  ke format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,13 +5,35 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
-//Membuat variabel untuk setiap elemen view
+/** @module Login-Script */
+/** 
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
-//Membuat variabel untuk menyimpan informasi email dan password
+/** 
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+
+/** 
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
 
 //Menambahkan aksi klik pada button
